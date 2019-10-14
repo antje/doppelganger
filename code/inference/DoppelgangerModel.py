9b134ac8a3fa72cf629cda84760fd285bf6da853
Fregly <cfregly@a483e78839c4.ant.amazon.com>
2019-10-13 18:52:38 -0700

    removed **kwargs to enable a/b testing

--- code/inference/DoppelgangerModel.py
+++ code/inference/DoppelgangerModel.py
@@ -10,16 +10,10 @@ class DoppelgangerModel(object):
             self.model = json.load(fp) 
         print("\n** LOADED MODEL from pairwise_top_25.json **")
 
-    def predict(self, X, feature_names, **kwargs):
-        convert_image_to_width = 299
-        convert_image_to_height = 299
-
-# TODO:  Change this logic to retrieve the 25 similar images accordingly
-
-        profile_pic = './images/%s_0.png' % str(int(X[0]))
+    def predict(self, X, feature_names):
         similar_image_arr = self.model[str(int(X[0]))]
         return similar_image_arr
 
 if __name__== "__main__":
     model = DoppelgangerModel()
-    print(model.predict([0], ['member_id']))
+    print(model.predict([0], ['image_id']))
